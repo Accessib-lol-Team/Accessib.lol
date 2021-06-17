83b365e2fe9b800a2e17c7c5d52092ddd4aa71fa
RIley Abrahamson <32375220+RileyAbr@users.noreply.github.com>
2021-06-16 22:36:42 -0500

    Created barebones LoginForm

---
+++ website/frontend/src/components/LoginForm/LoginForm.jsx
@@ -0,0 +1,13 @@
+const LoginForm = () => {
+    return (
+        <form>
+            <input type="email" />
+
+            <input type="password" />
+
+            <button type="submit">Login</button>
+        </form>
+    );
+};
+
+export default LoginForm;
