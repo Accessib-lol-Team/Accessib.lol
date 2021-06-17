eab866a48760635033215257e5f28fccee98bd27
RIley Abrahamson <32375220+RileyAbr@users.noreply.github.com>
2021-06-16 21:22:19 -0500

    First pass of styling for Home

--- website/frontend/src/pages/Home/Home.jsx
+++ website/frontend/src/pages/Home/Home.jsx
@@ -1,10 +1,40 @@
 import Header from "../../components/Header";
+import "./Home.css";
 
 const Home = () => {
     return (
         <div>
             <Header />
-            Home works!
+
+            <main>
+                <div>
+                    <p className="home--description">
+                        Accessib.lol was inspired by the lack of inclusive and
+                        accessibility features in many modern esports titles.
+                        Accessib.lol seeks to augment the League of Legends
+                        in-game experience to provide validation to members of
+                        the LGBTQ+ community and other minority groups.
+                    </p>
+                </div>
+
+                <div className="external-buttons">
+                    <button className="external-button">Download App</button>
+
+                    <a
+                        className="external-button"
+                        href="https://github.com/Accessib-lol-Team/Accessib.lol"
+                    >
+                        Github Repo
+                    </a>
+
+                    <a
+                        className="external-button"
+                        href="https://pridemakers.devpost.com/"
+                    >
+                        Hackathon Submission
+                    </a>
+                </div>
+            </main>
         </div>
     );
 };
