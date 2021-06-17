4e8f1eb25b2b6ad6e4119a36099ca0835786a074
RIley Abrahamson <32375220+RileyAbr@users.noreply.github.com>
2021-06-17 17:26:58 -0500

    Create Profile page
    Also shifted some consistent elements to the ContentPage component

--- website/frontend/src/components/ContentPage/ContentPage.jsx
+++ website/frontend/src/components/ContentPage/ContentPage.jsx
@@ -1,8 +1,12 @@
+import Header from "../../components/Header";
+import "./ContentPage.css";
+
 const ContentPage = (props) => {
     return (
         <div>
-            {/* Header */}
-            <main>{props.children}</main>
+            <Header />
+
+            <main class="content">{props.children}</main>
         </div>
     );
 };
