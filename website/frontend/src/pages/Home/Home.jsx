2049568380fb703d0a86334b4b1564564640fe4d
RIley Abrahamson <32375220+RileyAbr@users.noreply.github.com>
2021-06-16 21:36:47 -0500

    Finished styles and content for Home page

--- website/frontend/src/pages/Home/Home.jsx
+++ website/frontend/src/pages/Home/Home.jsx
@@ -3,39 +3,44 @@ import "./Home.css";
 
 const Home = () => {
     return (
-        <div>
+        <>
             <Header />
 
-            <main>
-                <div>
-                    <p className="home--description">
-                        Accessib.lol was inspired by the lack of inclusive and
-                        accessibility features in many modern esports titles.
-                        Accessib.lol seeks to augment the League of Legends
-                        in-game experience to provide validation to members of
-                        the LGBTQ+ community and other minority groups.
-                    </p>
-                </div>
+            <main className="home--content">
+                <section className="home--content--box">
+                    <div>
+                        <p className="home--description">
+                            Accessib.lol was inspired by the lack of inclusive
+                            and accessibility features in many modern esports
+                            titles. Accessib.lol seeks to augment the League of
+                            Legends in-game experience to provide validation to
+                            members of the LGBTQ+ community and other minority
+                            groups.
+                        </p>
+                    </div>
 
-                <div className="external-buttons">
-                    <button className="external-button">Download App</button>
+                    <div className="external-buttons">
+                        <button className="external-button">
+                            Download App
+                        </button>
 
-                    <a
-                        className="external-button"
-                        href="https://github.com/Accessib-lol-Team/Accessib.lol"
-                    >
-                        Github Repo
-                    </a>
+                        <a
+                            className="external-button"
+                            href="https://github.com/Accessib-lol-Team/Accessib.lol"
+                        >
+                            Github Repo
+                        </a>
 
-                    <a
-                        className="external-button"
-                        href="https://pridemakers.devpost.com/"
-                    >
-                        Hackathon Submission
-                    </a>
-                </div>
+                        <a
+                            className="external-button"
+                            href="https://pridemakers.devpost.com/"
+                        >
+                            Hackathon Submission
+                        </a>
+                    </div>
+                </section>
             </main>
-        </div>
+        </>
     );
 };
 
