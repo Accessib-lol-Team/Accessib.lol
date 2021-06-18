90f89696e1121100898977153abdae078306f634
RIley Abrahamson <32375220+RileyAbr@users.noreply.github.com>
2021-06-17 20:31:52 -0500

    Created api service layer
    Implemented auth-related functions

---
+++ website/frontend/src/services/api.services.js
@@ -0,0 +1,40 @@
+const apiURL = "https://giphy-gallery-server.herokuapp.com";
+
+const postLogin = async (data) =>
+    fetch(`${apiURL}/auth/login`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+    })
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error("Network response was not ok");
+            }
+            return response.json();
+        })
+        .then((data) => {
+            return data;
+        })
+        .catch((error) => {
+            return error;
+        });
+
+const postSignUp = async (data) =>
+    fetch(`${apiURL}/users`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+    })
+        .then((response) => response.json())
+        .then((data) => {
+            return data;
+        })
+        .catch((error) => {
+            return error;
+        });
+
+export { postLogin, postSignUp };
